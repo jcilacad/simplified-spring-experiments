e69b7f97291daf10f79291fd4ffe229ca9556a86
jcilacad <johnchristopherilacad27@gmail.com>
2024-09-10 00:15:34 +0800

    Added project demonstrating spring security with keycloak.

---
+++ keycloak-security/src/main/java/com/projects/keycloak_security/KeycloakSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.projects.keycloak_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSecurityApplication.class, args);
+	}
+
+}
---
+++ keycloak-security/src/test/java/com/projects/keycloak_security/KeycloakSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.keycloak_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
