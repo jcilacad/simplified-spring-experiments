7d99d78304d302d13ac98efd3811288cd99c1314
jcilacad <johnchristopherilacad27@gmail.com>
2024-12-31 23:38:09 +0800

    Added demo project demonstrating consuming SOAP api in spring boot.

---
+++ spring-consume-soap/src/main/java/com/projects/spring_consume_soap/SpringConsumeSoapApplication.java
@@ -0,0 +1,28 @@
+package com.projects.spring_consume_soap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringConsumeSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsumeSoapApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner lookup(CountryClient countryClient) {
+		return args -> {
+			String country = "Spain";
+
+			if (args.length > 0) {
+				country = args[0];
+			}
+			GetCountryResponse response = countryClient.getCountry(country);
+			System.err.println(response.getCountry().getCurrency());
+		};
+	}
+
+}
---
+++ spring-consume-soap/src/test/java/com/projects/spring_consume_soap/SpringConsumeSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.spring_consume_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConsumeSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
