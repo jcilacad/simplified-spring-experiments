cddd64d359f4764a75d3e6671abcc0762580fe43
jcilacad <johnchristopherilacad27@gmail.com>
2025-01-05 21:43:25 +0800

    Added project demonstrating spring boot with htmx.

---
+++ spring-ascii-art-generator/src/main/java/com/projects/spring_ascii_art_generator/SpringAsciiArtGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.projects.spring_ascii_art_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsciiArtGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAsciiArtGeneratorApplication.class, args);
+	}
+
+}
---
+++ spring-ascii-art-generator/src/test/java/com/projects/spring_ascii_art_generator/SpringAsciiArtGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.spring_ascii_art_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAsciiArtGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
